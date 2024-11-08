a03ba04473d71ea38d3830e9e2192549c8cfc253
Hunain-baig <147654267+Hunain-baig@users.noreply.github.com>
2024-11-08 15:01:41 +0500

    fix profile image api

--- Routes/user.js
+++ Routes/user.js
@@ -11,6 +11,7 @@ const Company = require("../models/Company");
 const multer = require('multer');
 const randomstring = require('randomstring')
 const sendMail = require('../config/nodemailer')
+const path = require('path');
 
 
 // create --> user API
@@ -326,12 +327,13 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({storage});
 
 router.post("/uploadImage", tokenVerification, upload.single("profileImage"), async (req, res) => {
   try {
-   
-    const userId = req.userIdFromToken;  
+    console.log(req.file);  // Check if req.file is populated
+    
+    const userId = req.userIdFromToken;
 
     if (!userId) {
       return res.status(400).json({ message: "User ID is missing." });
@@ -341,13 +343,16 @@ router.post("/uploadImage", tokenVerification, upload.single("profileImage"), as
 
     if (!user) {
       return res.status(404).json({ message: "User not found." });
-
     }
 
-    const imagePath = req.file.path;  
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded." });
+    }
 
-    user.profilePicture = imagePath;  
-    await user.save();  
+    const imagePath = req.file.path;  // Make sure req.file is not undefined
+
+    user.profilePicture = imagePath;
+    await user.save();
 
     res.status(200).json({ message: "Profile picture uploaded successfully.", path: imagePath });
   } catch (error) {
@@ -355,6 +360,7 @@ router.post("/uploadImage", tokenVerification, upload.single("profileImage"), as
     res.status(500).json({ message: "Error uploading profile picture.", error: error.message });
   }
 });
+
 
 
 
@@ -399,10 +405,11 @@ router.post("/resetPassword", async (req, res) => {
 
     const user = await User.findOne({ token: token });
 
-    if (user) {
+    if (user) { 
+
       const updateData = await User.findByIdAndUpdate(
         { _id: user._id },
-        { $set: { password: hashedPassword, token: "" } },
+        { $set: { password: hashedPassword, token: ""} },
         { new: true }
       );
       res
