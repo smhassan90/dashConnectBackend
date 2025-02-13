f16f7a92f5aaf33bdcc424aa193fb01755f8289f
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-02-13 02:57:46 -0800

    appcors

--- index.js
+++ index.js
@@ -18,8 +18,22 @@ const employeeRoute = require('./Routes/employeeApi')
 // const url = `mongodb+srv://bilal:dashconnect123@cluster0.t9ett.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const url = `mongodb+srv://bilal:dashconnect123@cluster0.t9ett.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const corsOptions = {
+    origin: 'https://dashconnect2.vercel.app', // Allow only your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow credentials (cookies, auth tokens)
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
+
+// Handle preflight requests (OPTIONS)
+app.options('*', cors(corsOptions));
+
+
+
+
+// app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
