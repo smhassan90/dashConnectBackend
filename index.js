9d1c27aef81a8d865f75e9b5c4c71b019717a560
OsamaAbdulHafeez <osamahafeez0987@gmail.com>
2025-03-06 13:31:45 +0500

    Update CORS configuration to allow requests from the production URL

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://datawise-six.vercel.app',
     credentials:true,
 };
 
